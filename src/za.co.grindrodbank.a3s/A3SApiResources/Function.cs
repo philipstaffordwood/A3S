e0ed2f678302cf0d01c6f640c1bdced09fb875e3
Victor Szewczenko <szewczenkov@gmail.com>
2019-11-06 11:07:56 +0200

    Re-generated all the license headers for the re-generated API classes.

--- src/za.co.grindrodbank.a3s/A3SApiResources/Function.cs
+++ src/za.co.grindrodbank.a3s/A3SApiResources/Function.cs
@@ -1,3 +1,9 @@
+/**
+ * *************************************************
+ * Copyright (c) 2019, Grindrod Bank Limited
+ * License MIT: https://opensource.org/licenses/MIT
+ * **************************************************
+ */
 /*
  * A3S
  *
