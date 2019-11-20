52c6a7c194aa2e980f5233ab50f0eb63a31a26c6
Paul van Coller <paul.vancoller@adeptadvisory.co.za>
2019-11-20 17:46:21 +0200

    New database structure

---
+++ db/sql/a3s/V1__a3s_v1.0.2.sql
@@ -0,0 +1,33 @@
+-- These updates relate to A3S v1.0.2 updates
+
+
+--
+-- Name: terms_of_service; Type: TABLE; Schema: _a3s; Owner: postgres
+--
+
+CREATE TABLE _a3s.terms_of_service (
+    id uuid NOT NULL,
+    agreement_name text NOT NULL,
+    version text NOT NULL,
+    agreement_file bytea NOT NULL,
+    changed_by uuid NOT NULL,
+    sys_period tstzrange DEFAULT tstzrange(CURRENT_TIMESTAMP, NULL::timestamp with time zone)
+);
+
+ALTER TABLE _a3s.terms_of_service OWNER TO postgres;
+
+COMMENT ON TABLE _a3s.terms_of_service IS 'Terms of service agreement entries that users agree to.';
+
+ALTER TABLE ONLY _a3s.terms_of_service
+    ADD CONSTRAINT pk_terms_of_service PRIMARY KEY (id);
+
+
+--
+-- Name: team; Type: TABLE; Schema: _a3s; Owner: postgres
+--
+
+ALTER TABLE _a3s.team
+    ADD terms_of_service_id uuid NULL;
+
+ALTER TABLE ONLY _a3s.team
+    ADD CONSTRAINT team_terms_of_service_id_fkey FOREIGN KEY (terms_of_service_id) REFERENCES _a3s.terms_of_service(id) ON DELETE RESTRICT;
