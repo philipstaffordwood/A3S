d1cbf7ab05117a0e7f3b9ed4064ad4d1a6e9287e
Paul van Coller <paul.vancoller@adeptadvisory.co.za>
2019-11-26 16:28:39 +0200

    Added new tables and not-null constraint updates to flyway script

--- db/sql/a3s/V1__terms_of_service.sql
+++ db/sql/a3s/V1__terms_of_service.sql
@@ -74,3 +74,130 @@ ALTER TABLE ONLY _a3s.team
     ADD CONSTRAINT fk_team_terms_of_service_terms_of_service_id FOREIGN KEY (terms_of_service_id) REFERENCES _a3s.terms_of_service(id) MATCH FULL ON DELETE RESTRICT;
 
 
+
+--
+-- Name: terms_of_service_user_acceptance; Type: TABLE; Schema: _a3s; Owner: postgres
+--
+
+CREATE TABLE _a3s.terms_of_service_user_acceptance (
+    terms_of_service_id uuid NOT NULL,
+    user_id text NOT NULL,
+    acceptance_time tstzrange NOT NULL
+);
+
+
+ALTER TABLE _a3s.terms_of_service_user_acceptance OWNER TO postgres;
+
+--
+-- Name: TABLE terms_of_service_user_acceptance; Type: COMMENT; Schema: _a3s; Owner: postgres
+--
+
+COMMENT ON TABLE _a3s.terms_of_service_user_acceptance IS 'This records the acceptance of terms of service entries by users.';
+
+
+--
+-- Name: COLUMN terms_of_service_user_acceptance.acceptance_time; Type: COMMENT; Schema: _a3s; Owner: postgres
+--
+
+COMMENT ON COLUMN _a3s.terms_of_service_user_acceptance.acceptance_time IS 'The date and time the user accepted the specific agreement.';
+
+
+--
+-- Name: terms_of_service_user_acceptance_history; Type: TABLE; Schema: _a3s; Owner: postgres
+--
+
+CREATE TABLE _a3s.terms_of_service_user_acceptance_history (
+    terms_of_service_id uuid NOT NULL,
+    user_id text NOT NULL,
+    acceptance_time tstzrange NOT NULL
+);
+
+
+ALTER TABLE _a3s.terms_of_service_user_acceptance_history OWNER TO postgres;
+
+--
+-- Name: TABLE terms_of_service_user_acceptance_history; Type: COMMENT; Schema: _a3s; Owner: postgres
+--
+
+COMMENT ON TABLE _a3s.terms_of_service_user_acceptance_history IS 'This stores the history of the acceptance of terms of service entries by users.
+On every update of a terms of service agreement version for a team, all user acceptance records get copied from ''terms_of_service_user_acceptance'' to ''terms_of_service_user_acceptance_history''.';
+
+--
+-- Name: COLUMN terms_of_service_user_acceptance_history.acceptance_time; Type: COMMENT; Schema: _a3s; Owner: postgres
+--
+
+COMMENT ON COLUMN _a3s.terms_of_service_user_acceptance_history.acceptance_time IS 'The date and time the user accepted the specific agreement.';
+
+--
+-- Name: terms_of_service_user_acceptance_history terms_of_service_user_acceptance_history_pk; Type: CONSTRAINT; Schema: _a3s; Owner: postgres
+--
+
+ALTER TABLE ONLY _a3s.terms_of_service_user_acceptance_history
+    ADD CONSTRAINT terms_of_service_user_acceptance_history_pk PRIMARY KEY (terms_of_service_id, user_id);
+
+
+--
+-- Name: terms_of_service_user_acceptance terms_of_service_user_acceptance_pk; Type: CONSTRAINT; Schema: _a3s; Owner: postgres
+--
+
+ALTER TABLE ONLY _a3s.terms_of_service_user_acceptance
+    ADD CONSTRAINT terms_of_service_user_acceptance_pk PRIMARY KEY (terms_of_service_id, user_id);
+
+--
+-- Name: terms_of_service_user_acceptance_history fk_terms_of_service_user_acceptance_history_terms_of_service_id; Type: FK CONSTRAINT; Schema: _a3s; Owner: postgres
+--
+
+ALTER TABLE ONLY _a3s.terms_of_service_user_acceptance_history
+    ADD CONSTRAINT fk_terms_of_service_user_acceptance_history_terms_of_service_id FOREIGN KEY (terms_of_service_id) REFERENCES _a3s.terms_of_service(id) MATCH FULL;
+
+
+--
+-- Name: terms_of_service_user_acceptance_history fk_terms_of_service_user_acceptance_history_user_user_id; Type: FK CONSTRAINT; Schema: _a3s; Owner: postgres
+--
+
+ALTER TABLE ONLY _a3s.terms_of_service_user_acceptance_history
+    ADD CONSTRAINT fk_terms_of_service_user_acceptance_history_user_user_id FOREIGN KEY (user_id) REFERENCES _a3s.application_user(id) MATCH FULL;
+
+
+--
+-- Name: terms_of_service_user_acceptance fk_terms_of_service_user_acceptance_terms_of_service_id; Type: FK CONSTRAINT; Schema: _a3s; Owner: postgres
+--
+
+ALTER TABLE ONLY _a3s.terms_of_service_user_acceptance
+    ADD CONSTRAINT fk_terms_of_service_user_acceptance_terms_of_service_id FOREIGN KEY (terms_of_service_id) REFERENCES _a3s.terms_of_service(id) MATCH FULL;
+
+
+--
+-- Name: terms_of_service_user_acceptance fk_terms_of_service_user_acceptance_user_user_id; Type: FK CONSTRAINT; Schema: _a3s; Owner: postgres
+--
+
+ALTER TABLE ONLY _a3s.terms_of_service_user_acceptance
+    ADD CONSTRAINT fk_terms_of_service_user_acceptance_user_user_id FOREIGN KEY (user_id) REFERENCES _a3s.application_user(id) MATCH FULL;
+
+
+--
+-- Adding of not-null constraints
+
+ALTER TABLE _a3s.application ALTER COLUMN name SET NOT NULL;
+ALTER TABLE _a3s.application ALTER COLUMN sys_period SET NOT NULL;
+ALTER TABLE _a3s.application_function ALTER COLUMN name SET NOT NULL;
+ALTER TABLE _a3s.application_function ALTER COLUMN application_id SET NOT NULL;
+ALTER TABLE _a3s.application_function ALTER COLUMN sys_period SET NOT NULL;
+ALTER TABLE _a3s.application_function_permission ALTER COLUMN sys_period SET NOT NULL;
+ALTER TABLE _a3s.function ALTER COLUMN name SET NOT NULL;
+ALTER TABLE _a3s.function ALTER COLUMN application_id SET NOT NULL;
+ALTER TABLE _a3s.function ALTER COLUMN sys_period SET NOT NULL;
+ALTER TABLE _a3s.function_permission ALTER COLUMN sys_period SET NOT NULL;
+ALTER TABLE _a3s.permission ALTER COLUMN name SET NOT NULL;
+ALTER TABLE _a3s.permission ALTER COLUMN sys_period SET NOT NULL;
+ALTER TABLE _a3s.role ALTER COLUMN name SET NOT NULL;
+ALTER TABLE _a3s.role ALTER COLUMN sys_period SET NOT NULL;
+ALTER TABLE _a3s.role_function ALTER COLUMN sys_period SET NOT NULL;
+ALTER TABLE _a3s.team ALTER COLUMN name SET NOT NULL;
+ALTER TABLE _a3s.team ALTER COLUMN sys_period SET NOT NULL;
+ALTER TABLE _a3s.user_role ALTER COLUMN sys_period SET NOT NULL;
+ALTER TABLE _a3s.user_team ALTER COLUMN sys_period SET NOT NULL;
+ALTER TABLE _a3s.team_team ALTER COLUMN sys_period SET NOT NULL;
+ALTER TABLE _a3s.role_role ALTER COLUMN sys_period SET NOT NULL;
+ALTER TABLE _a3s.application_data_policy ALTER COLUMN sys_period SET NOT NULL;
+ALTER TABLE _a3s.team_application_data_policy ALTER COLUMN sys_period SET NOT NULL;
